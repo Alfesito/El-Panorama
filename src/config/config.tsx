ad5da08ed1e1cc0ee5fd7edbb16148d047c087e1
Andrés Alfaro Fernández <andresalfe@gmail.com>
2026-01-07 09:02:18 +0100

    new trends json

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,6 +1,6 @@
 const CONFIG = {
     news_url: "https://raw.githubusercontent.com/Alfesito/ES-News-Topics/refs/heads/main/noticias_completas.json",
-    trends_url: "https://raw.githubusercontent.com/Alfesito/ES-News-Topics/main/trends_google%26x.json",
+    trends_url: "https://raw.githubusercontent.com/Alfesito/ES-News-Topics/refs/heads/main/tags_json/trends_google%26x.json",
     use_server: true,
     json_file: ''
   }
